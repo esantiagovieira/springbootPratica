3dc86342adeb4cdf6250eacc4d97c3925733ce01
Estevao Santiago <esantiagovieira@gmail.com>
2018-09-12 22:52:11 -0300

    Insere, atualiza e deleta clientes

---
+++ src/main/java/com/praticaspring/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,55 @@
+package com.praticaspring.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import com.praticaspring.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String nome;
+	@NotEmpty
+	@Email
+	private String email;
+	
+	public ClienteDTO() {}
+	
+	public ClienteDTO(Cliente obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
