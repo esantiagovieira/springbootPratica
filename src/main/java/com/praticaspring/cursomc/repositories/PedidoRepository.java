fd2a54406026befbc9295bb94648f66fda89852e
Estevao Santiago <esantiagovieira@gmail.com>
2018-09-27 18:42:21 -0300

    Restrição: Cliente recupera pedidos

--- src/main/java/com/praticaspring/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/praticaspring/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,18 @@
 package com.praticaspring.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.praticaspring.cursomc.domain.Cliente;
 import com.praticaspring.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
