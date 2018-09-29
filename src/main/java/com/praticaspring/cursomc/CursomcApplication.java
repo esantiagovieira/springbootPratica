3c746fe9afaad317b0cdf985bce980acc6a86f35
Estevao Santiago <esantiagovieira@gmail.com>
2018-09-29 15:12:19 -0300

    envio de imagem para AWS

--- src/main/java/com/praticaspring/cursomc/CursomcApplication.java
+++ src/main/java/com/praticaspring/cursomc/CursomcApplication.java
@@ -1,12 +1,17 @@
 package com.praticaspring.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.praticaspring.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3;
 
 	
 	public static void main(String[] args) {
@@ -16,6 +21,7 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
+		s3.uploadFile("C:\\temp\\teste.jpg");
 		
 	
 	}
