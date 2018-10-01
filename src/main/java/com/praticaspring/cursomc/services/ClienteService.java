1a164f9fef0186c886c8b0067c1fb7d6bf06d503
Estevao Santiago <esantiagovieira@gmail.com>
2018-10-01 20:47:03 -0300

    Usando padrao de noma para images

--- src/main/java/com/praticaspring/cursomc/services/ClienteService.java
+++ src/main/java/com/praticaspring/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.praticaspring.cursomc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -7,6 +8,7 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -43,6 +45,12 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String imagePrefix;
 
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -119,10 +127,8 @@ public class ClienteService {
 		if(user==null) {
 			throw new AuthorizationException("Acesso Negado");
 		}
-		URI uri = s3Service.uploadFile(mFile);
-		Cliente cli = find(user.getId());
-		cli.setImageUrl(uri.toString());
-		repo.save(cli);
-		return uri;
+		BufferedImage jpgImage = imageService.getJPGImageFromFile(mFile);
+		String fileName = imagePrefix + user.getId() + ".jpg";
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
 }
